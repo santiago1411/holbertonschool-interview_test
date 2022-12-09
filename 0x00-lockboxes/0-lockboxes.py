a5c5a837b0ebf58e22d7c6e628d112eb1c1e3703
santiago1411 <4547@holbertonschool.com>
2022-12-09 17:56:57 -0500

    unlock box

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
+    "method that determines if all the boxes can be opened."
+
+    "se recorren las posiciones de las cajas que se pueden abrir"
     for j in range(len(boxes)):
         if boxes[j]:
             continue
-        else:
+        else: #se evaluan la longitud total de las cajas con la longitud de las cajas abiertas
             if (j < len(boxes) - 1):
                 return False
     return True
