d8f8de04a037f0431c947d547ca05ace60bd86c1
santiago1411 <4547@holbertonschool.com>
2022-12-09 19:08:45 -0500

    unlock box

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,20 +3,20 @@
 
 
 def canUnlockAll(boxes):
-    """se recorren las posiciones de las cajas que se pueden abrir"""
-    for j in range(len(boxes)):
-        if boxes[j]:
-            continue
-        else:
-            if (j < len(boxes) - 1):
-                return False
-    return True
+    # """se recorren las posiciones de las cajas que se pueden abrir"""
+    # for j in range(len(boxes)):
+    #     if boxes[j]:
+    #         continue
+    #     else:
+    #         if (j < len(boxes) - 1):
+    #             return False
+    # return True
 
-    # lista_v = [0]
-    # for i in lista_v:
-    #     for j in boxes[i]:
-    #         if j < len(boxes) and j not in lista_v:
-    #             lista_v.append(j)
-    #     if len(lista_v) == len(boxes):
-    #         return True
-    # return False
+    lista_v = [0]
+    for i in lista_v:
+        for j in boxes[i]:
+            if j < len(boxes) and j not in lista_v:
+                lista_v.append(j)
+        if len(lista_v) == len(boxes):
+            return True
+    return False
